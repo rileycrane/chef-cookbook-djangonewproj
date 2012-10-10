beaa67fa62bb320fe47b19dbbfc136a41c9702b8
Joseph Bergantine <jbergantine@gmail.com>
2012-10-10 15:33:20 -0600

    Removed commented out line.

--- recipes/default.rb
+++ recipes/default.rb
@@ -62,7 +62,6 @@ end
 
 # Create the Database
 bash "create database" do
-  #user "vagrant"
   code <<-EOH
     echo "CREATE USER django_login WITH SUPERUSER PASSWORD 'secret';" | sudo -u postgres psql
     sudo -u postgres createdb -O django_login -E UTF8 --lc-ctype=en_US.utf8 --lc-collate=en_US.utf8 -T template0 django_db
