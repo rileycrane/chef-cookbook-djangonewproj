0a433b8819b127a72a54f48761c0310299c34a28
Joseph Bergantine <jbergantine@gmail.com>
2012-10-10 12:56:10 -0600

    Added sudo in front of git config commands.

--- recipes/default.rb
+++ recipes/default.rb
@@ -39,15 +39,15 @@ end
 bash "configure git" do
   user "vagrant"
   code <<-EOH
-    git config --global color.branch auto
-    git config --global color.diff auto
-    git config --global color.interactive auto
-    git config --global color.status auto
-    git config --global merge.summary true
-    git config --global alias.st status
-    git config --global alias.ci commit
-    git config --global alias.co checkout
-    git config --global alias.br branch
+    sudo git config --global color.branch auto
+    sudo git config --global color.diff auto
+    sudo git config --global color.interactive auto
+    sudo git config --global color.status auto
+    sudo git config --global merge.summary true
+    sudo git config --global alias.st status
+    sudo git config --global alias.ci commit
+    sudo git config --global alias.co checkout
+    sudo git config --global alias.br branch
   EOH
 end
 
