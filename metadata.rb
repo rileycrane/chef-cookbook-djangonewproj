cc24d944005c36d60045140ba593850dd6cedb99
rileycrane <rileycrane@gmail.com>
2013-04-06 16:47:59 -0300

    Create metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+name              "chef-cookbook-djangonewproj"
+maintainer        ""
+maintainer_email  ""
+license           ""
+description       "This cookbook is intended to be used with Django Newproj Template and the corresponding Vagrantfile."
